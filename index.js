330a91111f9e8a65abf6d5c9a843b76457a8ae7f
fatouuuu <fatmaabdullrahim@gmail.com>
2022-11-14 15:28:35 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+
+ function writeCards(names) {
+ 
+  let giftCards = []
+  for ( let i = 0; i < names.length; i++ ) 
+  {
+    giftCards.push( `Thank you, ${names[i]}, for the wonderful surprise gift!` )
+  }
+  return giftCards
+}
+
+function countDown(){
+    let posInt = 11; 
+    while (posInt--){
+        console.log(posInt)
+    }
+}
